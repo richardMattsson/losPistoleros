2e22040b541faf531b09a7ddde0fab86bd666127
richardMattsson <mattsson.richard@gmail.com>
2025-01-31 10:36:59 +0100

    adding a screenshot

--- image.js
+++ image.js
@@ -4,7 +4,7 @@ execute().catch(console.error);
 
 async function execute() {
   let image = await Image.load('los-pistoleros-logo.jpeg');
-  let newSize = image.resize({ width: 192, height: 192 }); // resize the image, forcing a width of 200 pixels. The height is computed automatically to preserve the aspect ratio.
+  let newSize = image.resize({ width: 1200, height: 800 }); // resize the image, forcing a width of 200 pixels. The height is computed automatically to preserve the aspect ratio.
   //.rotate(30); // rotate the image clockwise by 30 degrees.
-  return newSize.save('los-pistoleros-logo.png');
+  return newSize.save('screenshot.png');
 }
