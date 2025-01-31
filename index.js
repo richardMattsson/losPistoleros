a12c4f8c2f8188f6b705118e1e5acde19d2b4f3a
richardMattsson <mattsson.richard@gmail.com>
2025-01-31 11:28:28 +0100

    new url ending

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/service-worker.js')
+    .register('/losPistoleros/service-worker.js')
     .then((reg) => console.log('Service Worker registered', reg))
     .catch((err) => console.log('Service Worker error', err));
 }
