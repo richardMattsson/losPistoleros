a8284f0e9e3bf6e43daf9878caa57dd0470cfd49
richardMattsson <mattsson.richard@gmail.com>
2025-01-31 10:50:57 +0100

    new mobile screenshot

--- image.js
+++ image.js
@@ -3,8 +3,8 @@ const { Image } = require('image-js');
 execute().catch(console.error);
 
 async function execute() {
-  let image = await Image.load('los-pistoleros-logo.jpeg');
-  let newSize = image.resize({ width: 1200, height: 800 }); // resize the image, forcing a width of 200 pixels. The height is computed automatically to preserve the aspect ratio.
+  let image = await Image.load('mobile-screenshot.png');
+  let newSize = image.resize({ width: 360, height: 640 }); // resize the image, forcing a width of 200 pixels. The height is computed automatically to preserve the aspect ratio.
   //.rotate(30); // rotate the image clockwise by 30 degrees.
-  return newSize.save('screenshot.png');
+  return newSize.save('new-mobile-screenshot.png');
 }
